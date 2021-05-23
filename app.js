da65625d94358559adec8b79c8f0bad1d9df5507
Li, Matthew <mli679@gatech.edu>
2021-05-23 12:19:05 -0400

    Fix recording bug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html/"));
 });
 
 app.listen(PORT, () => {
