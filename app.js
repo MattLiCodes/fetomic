668bcd3a8f1e2a413c21397f860bda7811a1310d
Li, Matthew <mli679@gatech.edu>
2021-05-24 12:36:14 -0400

    Move js and css to separate files

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const app = require("express")();
+const express = require("express");
 const path = require("path");
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html/"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT, () => {
   console.log("Listening at port " + PORT);
