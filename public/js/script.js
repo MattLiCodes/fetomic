322dc441d6dcd04aa68bbdecbd9f4ea906b1e20a
Li, Matthew <mli679@gatech.edu>
2021-05-24 12:51:04 -0400

    Test js

--- public/js/script.js
+++ public/js/script.js
@@ -20,7 +20,8 @@ pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
   console.log("recordButton clicked");
-
+  //update the format
+  document.getElementById("title").innerHTML = "Hi";
   /*
 		Simple constraints object, for more advanced audio features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
